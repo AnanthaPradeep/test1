62f281a500986c6e23c31080b0cddfb2145d693e
AnanthaPradeep <anandhpradeep909@gmail.com>
2024-06-03 04:22:04 +0000

    fourth commit

--- nodeapp.js
+++ nodeapp.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer((request, response) => {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Hello, AnanthaPradeep\n');
+    response.end('Hello - welcome all\n');
 }) .listen(8000, () => {
     console.log('Server running at http://localhost:8000/');
 });
